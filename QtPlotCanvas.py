6acca11ebe2423320d5f418d4d25c7a36ed7cd10
pmazur <piotr.mazur@vmlyr.com>
2020-09-30 23:18:43 +0200

    Cleanup

--- QtPlotCanvas.py
+++ QtPlotCanvas.py
@@ -1,12 +1,5 @@
-from abc import abstractmethod
-
-from PyQt5 import QtCore
-from PyQt5.QtCore import QSize, QPointF, QRectF
-from PyQt5.QtGui import QMouseEvent
+from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QWidget
-
-from iplotlib.Plot import Plot
-from iplotlib.ui.Event import Event, MouseEvent
 
 """
 Main abstraction of a Qt plot canvas.
@@ -16,22 +9,8 @@ A Qt plot canvas is used to plot multiple plots and present them in form of a Qt
 
 class QtPlotCanvas(QWidget):
 
-    def createEvent(self,event):
-        ret = MouseEvent()
-        if event.type() == QtCore.QEvent.MouseMove:
-            ret.type = "mouseMove"
-            ret.x = event.x()
-            ret.y = event.y()
-        elif event.type() == QtCore.QEvent.Enter:
-            ret.type = "mouseEnter"
-        elif event.type() == QtCore.QEvent.Leave:
-            ret.type = "mouseLeave"
-        else:
-            # print("Unknown event: " + str(event.type()))
-            pass
-        return ret
-
     # This will be applied as initial size
+    #TODO: Can be directly related to the size of the canvas grid
     def sizeHint(self):
         return QSize(900, 400)
 
