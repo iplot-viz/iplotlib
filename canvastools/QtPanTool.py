13502d58ff9dbcd78e7347588ca7d5aa9f8bd22a
pmazur <piotr.mazur@vmlyr.com>
2020-10-02 13:07:51 +0200

    Cleanup

--- canvastools/QtPanTool.py
+++ canvastools/QtPanTool.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore
-from PyQt5.QtCore import QPoint, Qt
+from PyQt5.QtCore import QPoint, Qt, QPointF
 from PyQt5.QtGui import QPainter
 
 from qt.QtOverlayCanvasTool import QtOverlayCanvasTool
@@ -10,7 +10,7 @@ class QtOverlayPanTool(QtOverlayCanvasTool):
 
     def __init__(self):
         self.mouse_pressed: bool = False
-        self.move_start: QPoint = None
+        self.move_start: QPointF = None
 
     def process_paint(self, painter: QPainter):
         pass
@@ -18,13 +18,11 @@ class QtOverlayPanTool(QtOverlayCanvasTool):
     def process_event(self, canvas: QtPlotCanvas, event):
         if event.type() == QtCore.QEvent.MouseMove:
             if self.mouse_pressed:
-                pos = canvas._gnuplot_canvas.to_graph(event.localPos().x(), event.localPos().y())
-                dx = self.move_start[0] - pos[0]
-                dy = self.move_start[1] - pos[1]
-                print("Pan tool: moving by " + str(dy))
-                b = canvas._gnuplot_canvas.plot_range
-
-                canvas._gnuplot_canvas.set_bounds(b[0]+dx, b[1]+dy, b[2]+dx, b[3]+dy)
+                pos = QPointF(*canvas._gnuplot_canvas.to_graph(event.localPos().x(), event.localPos().y()))
+                delta = self.move_start - pos
+                bounds = canvas._gnuplot_canvas.plot_range
+                print("Pan tool: moving by " + str(delta) + " Current: " + str(bounds))
+                canvas._gnuplot_canvas.set_bounds(bounds[0]+delta.x(), bounds[1]+delta.y(), bounds[2]+delta.x(), bounds[3]+delta.y())
             return True
 
         elif event.type() == QtCore.QEvent.Enter:
@@ -36,15 +34,13 @@ class QtOverlayPanTool(QtOverlayCanvasTool):
 
         elif event.type() == QtCore.QEvent.MouseButtonPress:
             if event.button() == Qt.LeftButton:
-                self.move_start = canvas._gnuplot_canvas.to_graph(event.localPos().x(), event.localPos().y())
+                self.move_start = QPointF(*canvas._gnuplot_canvas.to_graph(event.localPos().x(), event.localPos().y()))
                 self.mouse_pressed = True
             return True
 
         elif event.type() == QtCore.QEvent.MouseButtonRelease:
             if event.button() == Qt.LeftButton:
                 self.mouse_pressed = False
-            else:
-                self.__reset(canvas)
             return True
 
         elif event.type() == QtCore.QEvent.MouseButtonDblClick:
@@ -53,11 +49,3 @@ class QtOverlayPanTool(QtOverlayCanvasTool):
         elif event.type() != 12:
             # print("UNKNOWN EVENT " + str(event.type()))
             pass
-
-    @staticmethod
-    def __reset(canvas: QtPlotCanvas):
-        for c_axis, plot in zip(canvas.axes_list(), canvas.plots()):
-            for index, axis in enumerate(plot.axes):
-                c_axis[index].begin = axis.begin
-                c_axis[index].end = axis.end
-        canvas.replot()
