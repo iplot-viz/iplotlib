d4989d928a17a070f5b2856a82fb3e14b6b276b7
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-07-30 09:40:15 +0200

    Remove VTK from environment. Do not set QT_API

--- environ.sh
+++ environ.sh
@@ -8,8 +8,6 @@ module load DataAccess/0.0.0-foss-2020b # brings in [proc/0.0.0, logging2/0.0.0,
 
 # Graphics backend requirements
 module load matplotlib/3.3.3-foss-2020b
-module load VTK/10.0.0.rc0-foss-2020b
 
 # Graphical User Interface backends
 module load QtPy/1.9.0-GCCcore-10.2.0
-export QT_API=pyside2
