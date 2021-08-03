b8c10bf9f6aab1e72ff52d04e16cbc4ee4045a4b
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-08-03 16:00:56 +0200

    Add CI scripts for pip install.

---
+++ ci-build/st01-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Bamboo script
+# Stage 1 : Pip install
+
+# Set up environment
+source ci-build/st00-header.sh $* || exit 1
+
+# Create a virtualized environment for installing iplotlib
+if [ -d "${PREFIX_DIR}" ];
+then
+    try rm -r ${PREFIX_DIR}
+fi
+
+try mkdir ${PREFIX_DIR}
+
+# Test install command
+try python3 -m pip --disable-pip-version-check install . --prefix=${PREFIX_DIR}
+
+export PYTHONPATH=${PYTHONPATH}:$(get_abs_filename "./${PREFIX_DIR}")
+try python3 -c "import iplotlib"
--- environ.sh
+++ environ.sh
@@ -1,13 +1,67 @@
 #!/bin/bash
 
+# 3-fingered-claw 
+function yell () 
+{ 
+  echo "$0: $*" >&2
+}
+
+function die () 
+{ 
+  yell "$*"; exit 1
+}
+
+function try () 
+{ 
+  "$@" || die "cannot $*" 
+}
+
+
+# Default to foss toolchain
+if [[ "$1" == "foss" || -z $1 ]];
+then
+    toolchain=foss
+elif [[ "$1" == "intel" ]];
+then
+    toolchain=intel
+fi
+echo "Toolchain: $toolchain"
+
 # Clean slate
-module purge
+try module purge
 
-# Data Access library
-module load DataAccess/0.0.0-foss-2020b # brings in [proc/0.0.0, logging2/0.0.0, uda-ccs/2.1.2]-foss-2020b
+case $toolchain in
 
-# Graphics backend requirements
-module load matplotlib/3.3.3-foss-2020b
+  "foss")
+    # Other IDV components
+    try module load iplotLogging/0.0.0-GCCcore-10.2.0
+    try module load iplotProcessing/0.0.0-foss-2020b
+    try module load iplotDataAccess/0.0.0-foss-2020b
 
-# Graphical User Interface backends
-module load QtPy/1.9.0-GCCcore-10.2.0
+    # Graphics backend requirements
+    try module load matplotlib/3.3.3-foss-2020b
+    try module load VTK/20210803-foss-2020b
+
+    # Graphical User Interface backends
+    try module load QtPy/1.9.0-GCCcore-10.2.0
+    ;;
+
+  "intel")
+    # Other IDV components
+    try module load iplotLogging/0.0.0-GCCcore-10.2.0
+    try module load iplotProcessing/0.0.0-intel-2020b
+    try module load iplotDataAccess/0.0.0-intel-2020b
+
+    # Graphics backend requirements
+    try module load matplotlib/3.3.3-intel-2020b
+    try module load VTK/20210803-intel-2020b
+
+    # Graphical User Interface backends
+    try module load QtPy/1.9.0-GCCcore-10.2.0
+    ;;
+   *)
+    echo "Unknown toolchain $toolchain"
+    ;;
+esac
+
+try module list -t 2>&1
