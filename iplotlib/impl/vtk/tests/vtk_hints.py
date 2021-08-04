59141b9c3f019e4ab45e29ee4ff8bf8c33f52a17
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-08-04 17:42:50 +0200

    Check if platform is headless

--- iplotlib/impl/vtk/tests/vtk_hints.py
+++ iplotlib/impl/vtk/tests/vtk_hints.py
@@ -1,8 +1,11 @@
+import os
 from vtkmodules import vtkRenderingOpenGL2
 
+def platform_is_headless() -> bool:
+    return os.getenv("DISPLAY") is None
 
 def vtk_is_headless() -> bool:
-    return \
+    return platform_is_headless() or \
         not hasattr(vtkRenderingOpenGL2, "vtkXOpenGLRenderWindow") and \
         not hasattr(vtkRenderingOpenGL2, "vtkWin32OpenGLRenderWindow") and \
         not hasattr(vtkRenderingOpenGL2, "vtkCocoaOpenGLRenderWindow") and \
