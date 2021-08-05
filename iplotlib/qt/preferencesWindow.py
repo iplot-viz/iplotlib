6f2f60fcaa7a75d073f98c1c494cf350d40a9637
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-08-05 17:21:54 +0200

    fix import

--- iplotlib/qt/preferencesWindow.py
+++ iplotlib/qt/preferencesWindow.py
@@ -9,6 +9,9 @@ from qtpy.QtWidgets import QApplication, QCheckBox, QColorDialog, QComboBox, QDa
     QStackedWidget, QTreeView, QVBoxLayout, QWidget
 
 from iplotlib.impl.matplotlib.matplotlibCanvas import ConversionHelper
+from iplotlib.core.axis import Axis
+from iplotlib.core.signal import ArraySignal
+from iplotlib.core.canvas import Canvas
 from iplotlib.core.axis import LinearAxis
 from iplotlib.core.canvas import Canvas
 from iplotlib.core.plot import PlotXY
