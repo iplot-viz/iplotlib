785299cf3e5ee713fde73f2a7c52984e19761338
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-10-17 13:56:55 +0200

    Add initial examples for using iplotlib framework.

---
+++ iplotlib/examples/customData.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import os
+import numpy as np
+from iplotlib.core import SimpleSignal, Canvas, PlotXY
+
+def plot():
+
+    # 1-D plot of Rosenbrock function @ y=2
+    x = np.linspace(-1, 1, 1000)
+    y = (1 - x ** 2) + 100 * (2 - x ** 2) ** 2
+    s = SimpleSignal(title='signal_1', x_data=x, y_data=y)
+
+    # Setup the graphics objects for plotting.
+    c = Canvas(rows=3, title=os.path.basename(__file__).replace('.py', ''))
+    p = PlotXY()
+    p.add_signal(s)
+    c.add_plot(p)
+
+    return c
---
+++ iplotlib/examples/stackLayout.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+from iplotlib.core import SimpleSignal, Canvas, PlotXY
+
+def plot():
+    ts = 1_000_000_000
+    te = ts + 8 * 32
+    xs = np.arange(ts, te, 8, dtype=np.uint64)
+    ys = np.sin(np.linspace(-1, 1, len(xs)))
+
+    # A 2col x 2row canvas
+    canvas = Canvas(2, 2, title=os.path.basename(__file__).replace('.py', ''))
+
+    # A plot in top-left with 1 signal.
+    signal11 = SimpleSignal(title="Signal1.1", hi_precision_data=True)
+    signal11.set_data([xs, ys])
+    plot11 = PlotXY(title="DateTime=True, HiPrecision=True")
+    plot11.axes[0].is_date = True
+    plot11.add_signal(signal11)
+    canvas.add_plot(plot11, 0)
+
+    # A plot in bottom-left with 2 stacked signal.
+    signal121 = SimpleSignal(title="Signal1.2.1")
+    signal121.set_data([xs, ys])
+    plot12 = PlotXY(title="DateTime=True, HiPrecision=True",
+                    hi_precision_data=True)
+    plot12.axes[0].is_date = True
+    plot12.add_signal(signal121)
+    signal122 = SimpleSignal(title="Signal1.2.2")
+    signal122.set_data([xs, ys + np.sin(xs)])
+    plot12.add_signal(signal122, 2)
+    canvas.add_plot(plot12, 0)
+
+    # A plot in top-right with 1 signal.
+    signal21 = SimpleSignal(title="Signal2.1", hi_precision_data=True)
+    signal21.set_data([xs, ys])
+    plot21 = PlotXY(title="DateTime=True, HiPrecision=True")
+    plot21.axes[0].is_date = True
+    plot21.add_signal(signal21)
+    canvas.add_plot(plot21, 1)
+
+    # A plot in bottom-right with 1 signal.
+    signal22 = SimpleSignal(title="Signal2.2")
+    signal22.set_data([xs, ys])
+    plot22 = PlotXY(title="DateTime=False, HiPrecision=True",
+                    hi_precision_data=True)
+    plot22.add_signal(signal22)
+    canvas.add_plot(plot22, 1)
+
+    return canvas
