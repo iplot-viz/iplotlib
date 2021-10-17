530e7d39a35c82103dea7d1fd49c43e146f178fd
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-10-17 13:57:11 +0200

    Use CanvasFactory.

--- iplotlib/qt/gui/__init__.py
+++
@@ -1,9 +0,0 @@
-from .iplotQtCanvas import IplotQtCanvas
-
-class IplotQtCanvasFactory:
-    def new(backend: str, *args, **kwargs) -> IplotQtCanvas:
-        if backend.lower() in ["matplotlib", "mpl", "mplot", "mplib"]:
-            return None
-        elif backend.lower() in ["vtk"]:
-            from iplotlib.impl.vtk.qt import QtVTKCanvas
-            return QtVTKCanvas(*args, **kwargs)
