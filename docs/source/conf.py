c10f40cb41cd38ff362c5153d4e03d4cbf1dbaac
Paulo Abreu <Paulo.TriboletdeAbreu@iter.org>
2022-05-30 16:51:30 +0200

    sphinx: removing numpy from mock imports, it was causing the doc to fail; including examples folder in the pattern search.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,8 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['iplotlib/examples']
+#exclude_patterns = ['iplotlib/examples']
+exclude_patterns = ['']
 
 # The master toctree document.
 master_doc = 'index'
@@ -142,4 +143,5 @@ napoleon_use_rtype = False
 
 autodoc_member_order = 'bysource'
 # this is in order to support numpy and friends documentation on RTD
-autodoc_mock_imports = ['pandas', 'numpy',  'scipy', 'iplotLogging', 'matplotlib', 'vtk']
+#autodoc_mock_imports = ['pandas', 'numpy',  'scipy', 'iplotLogging', 'matplotlib', 'vtk']
+autodoc_mock_imports = ['pandas', 'scipy', 'iplotLogging', 'matplotlib', 'vtk']
