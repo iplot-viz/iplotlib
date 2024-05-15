f4294a2c144a40452233c9ac770b94f737f76b1f
martinp6 <pmartin@minsait.com>
2024-05-15 09:19:45 +0000

    IDV-532 When its in undo mode disable the shared_x_axis mode for that execution

--- iplotlib/impl/matplotlib/matplotlibCanvas.py
+++ iplotlib/impl/matplotlib/matplotlibCanvas.py
@@ -395,7 +395,7 @@ class MatplotlibParser(BackendParserBase):
     def _axis_update_callback(self, mpl_axes):
 
         affected_axes = mpl_axes.get_shared_x_axes().get_siblings(mpl_axes)
-        if self.canvas.shared_x_axis:
+        if self.canvas.shared_x_axis and not self.canvas.undo:
             other_axes = self._get_all_shared_axes(mpl_axes)
             for other_axis in other_axes:
                 cur_x_limits = self.get_oaw_axis_limits(mpl_axes, 0)
