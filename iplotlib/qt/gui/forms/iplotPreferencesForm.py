15fa798c2953b925e57ea7457a6ae30ae6a82677
cabanij <jscabanilla@minsait.com>
2024-10-16 17:09:22 +0200

    IDV-636 Fixed wheel event that changed combobox and spinbox values inside the properties window

--- iplotlib/qt/gui/forms/iplotPreferencesForm.py
+++ iplotlib/qt/gui/forms/iplotPreferencesForm.py
@@ -110,6 +110,8 @@ class IplotPreferencesForm(QWidget):
     @staticmethod
     def create_spinbox(**params):
         widget = QSpinBox()
+        # Override the wheelEvent() method using a lambda function to ignore mouse wheel events
+        widget.wheelEvent = lambda event: event.ignore()
         if params.get("min"):
             widget.setMinimum(params.get("min"))
         if params.get("max"):
@@ -119,6 +121,8 @@ class IplotPreferencesForm(QWidget):
     @staticmethod
     def create_combo_box(items):
         widget = QComboBox()
+        # Override the wheelEvent() method using a lambda function to ignore mouse wheel events
+        widget.wheelEvent = lambda event: event.ignore()
         if isinstance(items, dict):
             for k, v in items.items():
                 widget.addItem(v, k)
