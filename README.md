803eb7630b0a7d03425ccc4bf8f400f92e42cadb
cabanij <jscabanilla@minsait.com>
2025-03-26 17:11:53 +0100

    IDV-675 The dependencies of each module have been moved to the pyproject.toml file, minimizing also the setup.py file and centering the configuration in the pyproject.toml file.

--- README.md
+++ README.md
@@ -54,21 +54,15 @@ The remaining steps below are not required if your are developing iplotlib.
     cd iplotlib
     source environ.sh  # loads reuired modules on sdcc
     ```
-3. Install requirements.
-    For strict versioning:
-    ```bash
-    pip install -r requirements.txt
-    ```
-
-4. Install `iplotlib`
+3. Install `iplotlib`
     ``` bash
     pip install --user .
     ```
-5. For developer installation (no copy)
+4. For developer installation (no copy)
     ``` bash
     pip install --user -e .
     ```
-6. For system-wide installs with Easybuild
+5. For system-wide installs with Easybuild
     1. Use PythonPackage easyblock with `use_pip=True`
     2. Prepare module file with dependencies from environ.sh
 
--- setup.py
+++ setup.py
@@ -15,36 +15,6 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="iplotlib",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author="Panchumarti Jaswant EXT",
-    author_email="jaswant.panchumarti@iter.org",
-    description="ITER plotting library",
-    long_description=long_description,
-    url="https://git.iter.org/scm/vis/iplotlib.git",
-    project_urls={
-        "Bug Tracker": "https://jira.iter.org/issues/?jql=project+%3D+IDV+AND+component+%3D+iplotlib",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        # "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "."},
-    packages=setuptools.find_packages(where="."),
-    python_requires=">=3.8",
-    install_requires=[
-        "iplotProcessing >= 0.4.0",
-        "matplotlib >= 3.5.1",
-        "pandas >= 1.1.4",
-        "PySide6 >= 6.2.3",
-        "vtk >= 9.1.0",
-    ],
-    entry_points={
-        "console_scripts": [
-            "iplotlib-qt-canvas = iplotlib.qt.gui.iplotQtStandaloneCanvas:main"
-        ]
-    },
-    package_data={"iplotlib.qt": ["icons/*.png", "icons/*.svg"]},
 )
