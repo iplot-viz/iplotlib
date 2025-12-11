4bb557c22cba76ebf6986a751e571c8823f9b77c
cabanij <jscabanilla@minsait.com>
2025-12-11 16:56:00 +0100

    IDV-732 Added the first PyQtGraph test based on CSV to validate data loading and processing. More cases will follow.

---
+++ iplotlib/impl/pyqtgraph/tests/test_pyqt.py
@@ -0,0 +1,132 @@
+import os
+import tempfile
+import unittest
+
+import numpy as np
+from iplotDataAccess.dataAccess import DataAccess
+from iplotlib.core.canvas import Canvas
+from iplotlib.impl.pyqtgraph.pyQtGraphCanvas import PyQtGraphParser
+from iplotlib.interface import AccessHelper
+
+ROOT = os.path.dirname(__file__)
+data_dir = os.path.join(ROOT, 'csv/ITER')
+
+dscfg_csv = """
+{
+    "csv": {
+        "path": "%s",
+        "type": "CSV"
+    }
+}
+"""
+
+
+class PyQtGraphTesting(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.da = DataAccess()
+        self.ds = "csv"
+
+        dscfg = dscfg_csv % data_dir
+
+        with tempfile.NamedTemporaryFile(mode='w+') as fp:
+            fp.write(dscfg)
+            fp.seek(0)
+            os.environ.update({'IPLOT_SOURCES_CONFIG': os.path.abspath(fp.name)})
+            if self.da.load_config(fp.name):
+                AccessHelper.da = self.da
+
+    # --------------------------
+    #           TESTS
+    # --------------------------
+
+    def test_01_null_refresh(self):
+        canvas = Canvas(0, 0)
+        self.pyqt_parser = PyQtGraphParser()
+        self.pyqt_parser.process_ipl_canvas(canvas)
+
+        size = self.pyqt_parser.figure.size()
+
+        self.assertEqual(0, 0)
+        self.assertEqual(0, 0)
+
+    def test_CSVAccessByPulse(self) -> None:
+        test_cases = [
+            {
+                "varname": "MAG-MCTB-F1:VAR1",
+                "pulse": "ITER:MCTB-TEST/111",
+                "expected_x_shape": (11,),
+                "expected_y_shape": (11,),
+                "expected_x_values": [-5.0, -4.0, -3.0, 0.0, 1.0, 1.1, 1.5, 2.0, 2.5, 3.0, 3.5],
+                "expected_y_values": [0, 1, 2, 10, 11, 12, 12, 12, 12, 14, 0]
+            },
+            {
+                "varname": "MAG-MCTB-F1:VAR2",
+                "pulse": "ITER:MCTB-TEST/111",
+                "expected_x_shape": (11,),
+                "expected_y_shape": (11,),
+                "expected_x_values": [-5.0, -4.0, -3.0, 0.0, 1.0, 1.1, 1.5, 2.0, 2.5, 3.0, 3.5],
+                "expected_y_values": [1, 2, 3, 11, 12, 13, 13, 13, 13, 15, 0],
+            },
+        ]
+
+        for test_case in test_cases:
+            varname = test_case["varname"]
+            pulse = test_case["pulse"]
+            expected_x_shape = test_case["expected_x_shape"]
+            expected_y_shape = test_case["expected_y_shape"]
+            expected_x_values = test_case["expected_x_values"]
+            expected_y_values = test_case["expected_y_values"]
+
+            dobj = self.da.get_data(self.ds, varname=varname, pulse=pulse)
+
+            self.assertEqual(np.shape(dobj.xdata), expected_x_shape)
+            self.assertEqual(np.shape(dobj.ydata), expected_y_shape)
+            self.assertIsInstance(dobj.xdata, (np.ndarray, list))
+            self.assertIsInstance(dobj.ydata, (np.ndarray, list))
+
+    def test_CSVAccessByPulseWithTime(self) -> None:
+        test_cases = [
+            {
+                "varname": "MAG-MCTB-F1:VAR1",
+                "pulse": "ITER:MCTB-TEST/111",
+                "tsS": -3.0,
+                "tsE": 3.0,
+                "expected_x_shape": (8,),
+                "expected_y_shape": (8,),
+                "expected_x_values": [-3.0, 0.0, 1.0, 1.1, 1.5, 2.0, 2.5, 3.0, 3.5],
+                "expected_y_values": [2, 10, 11, 12, 12, 12, 12, 14]
+            },
+            {
+                "varname": "MAG-MCTB-F1:VAR2",
+                "pulse": "ITER:MCTB-TEST/111",
+                "tsS": 0.0,
+                "tsE": 2.0,
+                "expected_x_shape": (5,),
+                "expected_y_shape": (5,),
+                "expected_x_values": [0.0, 1.0, 1.1, 1.5, 2.0],
+                "expected_y_values": [10, 11, 12, 12, 12]
+            }
+        ]
+
+        for test_case in test_cases:
+            varname = test_case["varname"]
+            pulse = test_case["pulse"]
+            tsS = test_case["tsS"]
+            tsE = test_case["tsE"]
+            expected_x_shape = test_case["expected_x_shape"]
+            expected_y_shape = test_case["expected_y_shape"]
+            expected_x_values = test_case["expected_x_values"]
+            expected_y_values = test_case["expected_y_values"]
+
+            dobj = self.da.get_data(self.ds, varname=varname, pulse=pulse, tsS=tsS, tsE=tsE)
+
+            self.assertEqual(np.shape(dobj.xdata), expected_x_shape)
+            self.assertEqual(np.shape(dobj.ydata), expected_y_shape)
+            self.assertIsInstance(dobj.xdata, (np.ndarray, list))
+            self.assertIsInstance(dobj.ydata, (np.ndarray, list))
+
+
+if __name__ == '__main__':
+    unittest.main()
